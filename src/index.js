0a98725d35258509690d7688c2381c048484748e
Enrique Briones <enr.briones@outlook.cl>
2020-09-28 03:58:25 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { CalendarApp } from './CalendarApp';
+
+import './styles.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <CalendarApp/>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
